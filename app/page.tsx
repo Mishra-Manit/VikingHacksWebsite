093b7049d7a153f7946bce8553813311f04eff56
Mishra-Manit <mshmanit@gmail.com>
2023-09-23 16:14:08 -0700

    manit changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,27 +13,27 @@ export default function HomePage() {
           href="#"
           target="_blank"
           rel="noreferrer"
-          className="border border-gray-600 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
+          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
         >
-          Now accepting {" "}
-          <span className="text-blue-600 hover:underline">sponsors</span>
+          Now Accepting {" "}
+          <span className="text-blue-600">Sponsorships!</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Create. {" "}
+          An IRL {" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">Innovate.</span>
+            <span className="relative">Hackathon</span>
           </span>{" "}
-          Elevate.
+          for everyone.
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-        Viking Hacks is Irvington High School&apos;s very own annual hackathon, bringing together 100+ students from around the bay to build innovative projects and present their work to the community.
+          Take part in the most exciting hackathon in the bay!
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Register for 2024!
+          Register Now!
         </Link>
         
       </main>
