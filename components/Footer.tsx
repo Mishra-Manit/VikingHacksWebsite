62727e2992f16b182d0e5e140d75cbcf3a37f37c
laneric <ericlan@erics-mbp.lan>
2023-09-23 12:54:57 -0700

    change icon destinations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,19 +33,14 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://ihsvikings.org"
           className="group"
           aria-label="TaxPal on Twitter"
         >
-          <svg
-            aria-hidden="true"
-            className="h-6 w-6 fill-gray-500 group-hover:fill-gray-300"
-          >
-            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
-          </svg>
+          <svg className="h-6 w-6 fill-gray-500 group-hover:fill-gray-300" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,16V88H40V56Z"></path></svg>
         </Link>
         <Link
-          href="https://github.com/Nutlope/roomGPT"
+          href="https://github.com/viking-hacks"
           className="group"
           aria-label="TaxPal on GitHub"
         >
