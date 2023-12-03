9c2f7be964dcb4de946cbac29e932628e365e1b7
Mishra-Manit <mshmanit@gmail.com>
2023-12-03 12:09:06 -0800

    stop crashing

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,17 +1,5 @@
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Your redirect logic here
-  return {
-    redirect: {
-      destination: 'https://youtube.com',
-      permanent: false,
-    },
-  };
-};
-
 const RegisterPage = () => {
-  return <p>Redirecting...</p>;
+  return <p>Coming Soon...</p>;
 };
 
 export default RegisterPage;
