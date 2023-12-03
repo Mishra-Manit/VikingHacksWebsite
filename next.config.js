0ec578de255894f28b0eb81646324bb550544d18
Mishra-Manit <mshmanit@gmail.com>
2023-12-03 12:07:36 -0800

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects: async() => {
+    return [
+      {
+        source: '/pay',
+        destination: 'https://hcb.hackclub.com/donations/start/viking-hacks',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     domains: ["upcdn.io", "replicate.delivery"],
   },
