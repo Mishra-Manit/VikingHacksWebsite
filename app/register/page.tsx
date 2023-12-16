c6d3b1aaf809f97e896e6ab10c1d29d2a2895a5b
Mishra-Manit <mshmanit@gmail.com>
2023-12-16 15:25:13 -0800

    messsing with signup form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,13 @@
-const RegisterPage = () => {
-  return <p>Coming Soon...</p>;
+// ComingSoon.tsx
+import React from 'react';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h1>Coming Soon</h1>
+      <p>Stay tuned for updates!</p>
+    </div>
+  );
 };
 
-export default RegisterPage;
+export default ComingSoon;
