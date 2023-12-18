a96e2ea53eaa69c861a39d33d977ab5ae1408924
Mishra-Manit <mshmanit@gmail.com>
2023-12-17 23:16:23 -0800

    testing reg form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,14 @@
-// ComingSoon.tsx
+'use client'
+
 import React from 'react';
+import { Widget } from '@typeform/embed-react'
 
 const ComingSoon: React.FC = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+    <div>
       <h1>Coming Soon</h1>
       <p>Stay tuned for updates!</p>
+      <Widget id="01HHXVTFJX8GCDA5SXNYKZE620" style={{ width: '100%', height: '600px' }} className="my-form" />
     </div>
   );
 };
