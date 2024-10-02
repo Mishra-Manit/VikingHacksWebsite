0f4141443d9cc562722d2476d9cd2317a100ac81
laneric <ericlan707@gmail.com>
2024-10-01 21:13:41 -0700

    revamp stie

--- app/page.tsx
+++ app/page.tsx
@@ -6,35 +6,47 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import DottedLine from "../components/DottedLine";
 
+function ArrowIcon() {
+  return (
+    <svg aria-hidden="true" fill="none" height={21} viewBox="0 0 20 21" width={20} xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#arrow_icon)"><path clip-rule="evenodd" d="M7.25 9.75H6.5V11.25H7.25H11.4393L9.96967 12.7197L9.43934 13.25L10.5 14.3107L11.0303 13.7803L13.7803 11.0303C14.0732 10.7374 14.0732 10.2626 13.7803 9.96967L11.0303 7.21967L10.5 6.68934L9.43934 7.75L9.96967 8.28033L11.4393 9.75H7.25ZM10 17C6.41015 17 3.5 14.0899 3.5 10.5C3.5 6.91015 6.41015 4 10 4C13.5899 4 16.5 6.91015 16.5 10.5C16.5 14.0899 13.5899 17 10 17ZM2 10.5C2 14.9183 5.58172 18.5 10 18.5C14.4183 18.5 18 14.9183 18 10.5C18 6.08172 14.4183 2.5 10 2.5C5.58172 2.5 2 6.08172 2 10.5Z" fill="currentColor" fill-rule="evenodd"></path></g><defs><clipPath id="arrow_icon"><rect fill="currentColor" height="16" rx="0.25" width="16" x="2" y="2.5"></rect></clipPath></defs></svg>
+  );
+}
+
 export default function HomePage() {
   return (
     <>
-      <main className="min-h-screen h-full bg-black pb-10">
+      <main className="min-h-screen h-full bg-white pb-10">
         <Header />
-        <div className="border -mt-[1px] m-12 border-neutral-800 p-12">
+        <div className="m-12 mt-20">
           {/* main container */}
-          <div className="flex flex-col lg:flex-row border border-neutral-800 border-container p-1 h-[60vh] w-full z-[2]">
-            <div className="h-full w-full lg:w-[62%] overflow-hidden relative transition-all">
-              <Image
-                src={"/eventcenter1.jpeg"}
-                alt={"Fremont Downtown Event Center"}
-                fill
-                style={{
-                  objectFit: "cover",
-                  objectPosition: "50% 100%"
-                }}
-              />
-            </div>
-            <div className="h-full w-full lg:w-[38%] bg-neutral-300">
-
-            </div>
+          <div className="h-[60vh] w-full overflow-hidden relative transition-all">
+            <Image
+              src={"/eventcenter1.jpeg"}
+              alt={"Fremont Downtown Event Center"}
+              fill
+              style={{
+                objectFit: "cover",
+                objectPosition: "50% 100%"
+              }}
+            />
           </div>
-          <div className="items-center flex flex-col my-20">
-            <h1 className="font-mono text-[10vw] leading-none">$1,000,000</h1>
-            <h2 className="font-mono text-lg text-neutral-300">in prizes</h2>
+          <div className="flex flex-col lg:flex-row my-20 justify-between lg:items-center">
+            <div className="flex flex-col text-black">
+              <h1 className="tracking-[-0.07em] font-medium text-[9vw] sm:text-[8vw] lg:text-[5.5vw] leading-[1.1]">Viking Hacks is here.</h1>
+              <text className="font-mono text-[4vw] sm:text-[2.5vw] lg:text-[1.5vw] uppercase">January 10th - Fremont, CA.</text>
+            </div>
+            <Link href={"/register"} target={"_self"} className="mt-5 lg:mt-0 flex font-mono items-center justify-center gap-x-2 text-white bg-[rgb(0,87,255)] px-10 md:px-32 text-2xl py-1 h-24">
+              <div className="flex flex-col">
+                <div className="flex items-center justify-center">
+                  <span className="font-mono text-xl lg:text-2xl uppercase mr-2">Register Now</span>
+                  <svg aria-hidden="true" fill="none" height={23} viewBox="0 0 20 21" width={23} xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#arrow_icon)"><path clip-rule="evenodd" d="M7.25 9.75H6.5V11.25H7.25H11.4393L9.96967 12.7197L9.43934 13.25L10.5 14.3107L11.0303 13.7803L13.7803 11.0303C14.0732 10.7374 14.0732 10.2626 13.7803 9.96967L11.0303 7.21967L10.5 6.68934L9.43934 7.75L9.96967 8.28033L11.4393 9.75H7.25ZM10 17C6.41015 17 3.5 14.0899 3.5 10.5C3.5 6.91015 6.41015 4 10 4C13.5899 4 16.5 6.91015 16.5 10.5C16.5 14.0899 13.5899 17 10 17ZM2 10.5C2 14.9183 5.58172 18.5 10 18.5C14.4183 18.5 18 14.9183 18 10.5C18 6.08172 14.4183 2.5 10 2.5C5.58172 2.5 2 6.08172 2 10.5Z" fill="currentColor" fill-rule="evenodd"></path></g><defs><clipPath id="arrow_icon"><rect fill="currentColor" height="16" rx="0.25" width="16" x="2" y="2.5"></rect></clipPath></defs></svg>
+                </div>
+                <span className="block mt-1 font-mono font-medium leading-none text-center tracking-normal text-[14px] uppercase text-[#95B9FF]">completely free</span>
+              </div>
+            </Link>
           </div>
-          <DottedLine />
-          <section className="flex justify-between py-20 items-end font-sans font-normal">
+          <hr className="border-t border-dashed border-blue-300" />
+          <section className="flex justify-between text-black py-20 items-end font-sans font-normal">
             <span className="text-[32px] md:text-[64px]">Sponsors</span>
             <span className="text-[20px] md:text-[26px] font-light">Partners</span>
           </section>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 // fonts
 import { Pixelify_Sans } from 'next/font/google'
@@ -13,56 +14,100 @@ const navItems = {
     // 	name: 'REGISTER',
     // },
     '/register': {
-        name: 'REGISTER',
+        name: 'Register Now',
     },
 };
 
 // icons
-function ArrowIcon(props: { size: number, rotation: number }) {
-    let length = props.size.toString();
+function ArrowIcon() {
     return (
-        <svg
-            width={length}
-            height={length}
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={props.rotation ? `rotate-${props.rotation}` : ""}
-            >
-            <path
-                d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-                fill="currentColor"
-            />
-        </svg>
+        <svg aria-hidden="true" fill="none" height={21} viewBox="0 0 20 21" width={20} xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#arrow_icon)"><path clip-rule="evenodd" d="M7.25 9.75H6.5V11.25H7.25H11.4393L9.96967 12.7197L9.43934 13.25L10.5 14.3107L11.0303 13.7803L13.7803 11.0303C14.0732 10.7374 14.0732 10.2626 13.7803 9.96967L11.0303 7.21967L10.5 6.68934L9.43934 7.75L9.96967 8.28033L11.4393 9.75H7.25ZM10 17C6.41015 17 3.5 14.0899 3.5 10.5C3.5 6.91015 6.41015 4 10 4C13.5899 4 16.5 6.91015 16.5 10.5C16.5 14.0899 13.5899 17 10 17ZM2 10.5C2 14.9183 5.58172 18.5 10 18.5C14.4183 18.5 18 14.9183 18 10.5C18 6.08172 14.4183 2.5 10 2.5C5.58172 2.5 2 6.08172 2 10.5Z" fill="currentColor" fill-rule="evenodd"></path></g><defs><clipPath id="arrow_icon"><rect fill="currentColor" height="16" rx="0.25" width="16" x="2" y="2.5"></rect></clipPath></defs></svg>
     );
 }
 
-
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 bg-black px-12 py-6 flex items-center justify-between w-full border-b border-neutral-800">
-            <div className="hidden flex-1 grow md:block">
-                <span className="font-mono font-light text-sm text-neutral-400">FEB 4 2025, FREMONT</span>
+        <header className="sticky top-0 z-50 px-12 py-6 flex items-center justify-between w-full">
+            <div className="absolute top-0 w-full -mx-12 h-[200px]">
+                <div className="relative w-full h-full">
+                    <div
+                        className="absolute inset-0 z-1 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(0.12px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 12.5%, rgb(0, 0, 0) 25%, rgba(0, 0, 0, 0) 37.5%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-2 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(0.23px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 12.5%, rgb(0, 0, 0) 25%, rgb(0, 0, 0) 37.5%, rgba(0, 0, 0, 0) 50%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-3 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(0.47px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 25%, rgb(0, 0, 0) 37.5%, rgb(0, 0, 0) 50%, rgba(0, 0, 0, 0) 62.5%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-4 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(0.94px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 37.5%, rgb(0, 0, 0) 50%, rgb(0, 0, 0) 62.5%, rgba(0, 0, 0, 0) 75%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-5 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(1.88px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 50%, rgb(0, 0, 0) 62.5%, rgb(0, 0, 0) 75%, rgba(0, 0, 0, 0) 87.5%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-6 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(3.75px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 62.5%, rgb(0, 0, 0) 75%, rgb(0, 0, 0) 87.5%, rgba(0, 0, 0, 0) 100%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-7 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(7.5px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 75%, rgb(0, 0, 0) 87.5%, rgb(0, 0, 0) 100%)',
+                        }}
+                    ></div>
+                    <div
+                        className="absolute inset-0 z-8 pointer-events-none"
+                        style={{
+                            backdropFilter: 'blur(15px)',
+                            maskImage: 'linear-gradient(to top, rgba(0, 0, 0, 0) 87.5%, rgb(0, 0, 0) 100%)',
+                        }}
+                    ></div>
+                </div>
             </div>
-            <h1 className={`mx-auto text-lg ${pixelify_sans.className}`}>Viking Hacks</h1>
-            <div className="flex-grow flex-1 flex justify-end font-mono text-sm">
+            <div className="hidden flex-1 grow md:block z-10 select-none">
+                <div className="h-10 w-[6.2rem]">
+                    <Image src="/viking_logo_transparent.png" alt="Viking Hacks Logo" height={559} width={214} layout="responsive"/>
+                </div>
+            </div>
+            <div className="flex-grow flex-1 flex z-10 justify-end h-full font-mono font-light text-sm">
                 {Object.entries(navItems).map(([path, { name }]) => {
                     return (
                         <Link key={path} href={path} target={"_self"} className="
                         flex
                         items-center
                         gap-x-2
-                        text-black
-                        bg-white
-                        rounded-lg
+                        text-white
+                        bg-[rgb(0,87,255)]
                         px-5
                         py-1
-                        hover:ring
-                        ring-neutral-700
-                        ring-offset-black
-                        ring-offset-2
+                        h-10
+                        uppercase
                         ">{name}
-                        <ArrowIcon rotation={45} size={10} />
+                        <ArrowIcon />
                         </Link>
                     );
                 })}
